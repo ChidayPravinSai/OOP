fcc52de3b5447f74d653457c4b004d07881ecb2c
ChidayPravinSai <2420030777@klh.edu.in>
2025-08-05 09:38:54 +0530

    Add files via upload

---
+++ Practical Lab/Week1/ExplicitTypeCasting.java
@@ -0,0 +1,14 @@
+package Examples;
+
+public class ExplicitTypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 6.90;
+		int b = (int) a;
+		System.out.println("Double value: " + a);
+        System.out.println("Integer value after casting: " + b);
+
+	}
+
+}
---
+++ Practical Lab/Week1/PrimitiveDataTypes.java
@@ -0,0 +1,26 @@
+package Examples;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a =10;
+		short b =100;
+		int c =1000;
+		long d =10000L;
+		float e = 10.5f;
+		double f = 20.09;
+		char g ='A';
+		boolean h =true;
+		
+		System.out.println("byte value: "+a);
+		System.out.println("short value: "+b);
+		System.out.println("int value: "+c);
+		System.out.println("long value: "+d);
+		System.out.println("float value: "+e);
+		System.out.println("double value: "+f);
+		System.out.println("char value: "+g);
+		System.out.println("boolean value: "+h);
+	}
+
+}
